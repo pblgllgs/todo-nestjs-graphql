a28a745cad828f9b5e8acb748b5e9422dc900151
pblgllgs <pbl.gllgs@gmail.com>
2022-11-22 22:49:44 -0300

    arguments, resolvers, code first

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,27 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver, Int, Args } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, {
+    description: 'Hello World es lo que retorna',
+    name: 'hello',
+  })
   helloWorld(): string {
     return 'Hello World';
+  }
+
+  @Query(() => Float, { name: 'randomNumber', description: 'Random number' })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, {
+    name: 'randomNumberFromZeroTo',
+    description: 'From zero to',
+  })
+  getRandomFromZeroTo(
+    @Args('to', { nullable: true, type: () => Int }) to = 6,
+  ): number {
+    return Math.floor(Math.random() * (to - 0) + 0);
   }
 }
